b3f828fe82be5c6fa619d756c5ceea6ab4ac50c4
Siyu Ren <sren@ualberta.ca>
2026-03-23 19:31:13 -0600

    Fix database path and include working dus.db with data

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Topic, Entry
+from .models import Warrantypolicy, Vehicle, Customer, Dealership
+
 
 admin.site.register(Topic)
 admin.site.register(Entry)
+
+
+admin.site.register(Warrantypolicy)
+admin.site.register(Vehicle)
+admin.site.register(Customer)
+admin.site.register(Dealership)
+
